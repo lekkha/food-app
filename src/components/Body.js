12e117c768999d16815623ec507d32ad4f87d044
Lekha <lekhasoni001@gmail.com>
2024-02-08 21:34:26 +0530

    building filter button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,57 @@
 import RestrauntCard from "./RestrauntCard";
-import resList from "../utils/mockData";
+// import resList from "../utils/mockData";
 
 const Body = () => {
+
+  const listOfRes = [
+    {
+    data: {
+      id: "74453",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+      cuisines: ["Pizzas"],
+      tags: [],
+      costForTwo: 40000,
+      deliveryTime: 45,
+
+    },
+  },
+  {
+    data: {
+      id: "74454",
+      name: "Domino's",
+      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+      cuisines: ["Pizzas"],
+      tags: [],
+      costForTwo: 40000,
+      deliveryTime: 45,
+
+    },
+  },
+  {
+    data: {
+      id: "74455",
+      name: "Domi",
+      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+      cuisines: ["Pizzas"],
+      tags: [],
+      costForTwo: 40000,
+      deliveryTime: 45,
+
+    },
+  },
+  ]; 
+
+
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button className="filter-btn">Top Rated Restraunts</button>
+        </div>
         <div className="res-contianer">
           {/* since resList is an arrya  */}
   
-          {resList.map((restraunt) => (
+          {listOfRes.map((restraunt) => (
             <RestrauntCard key={restraunt.data.id} resData={restraunt}/>
           ))}
   
