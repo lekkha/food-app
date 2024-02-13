6afedaf5a67b0476ae196973a17c8fd5b1167318
Lekha <lekhasoni001@gmail.com>
2024-02-13 13:39:02 +0530

    online status dot : using customHook useonlinestatus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import { LOGO_URL } from '../utils/constants';
 import { Link } from 'react-router-dom';
+import useOnlineStatus from '../utils/useOnlineStatus';
 
 const Header = () => {
 
   const [loginbtn, setLoginbtn] = useState("Login");
-
+  const onlineStatus = useOnlineStatus();
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +14,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status : {onlineStatus ? "🟢" : "🔴"}</li>
           <li><Link to="/" >Home</Link></li>
           <li><Link to="/about">About Us</Link></li>
           <li><Link to="/contact">Contact</Link></li>
