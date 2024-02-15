761a1e5acf1b38df5f716edc75772a59be0a2dcb
Lekha <lekhasoni001@gmail.com>
2024-02-15 19:33:11 +0530

    create store + cart slice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Outlet />
+    <Provider store={appStore} >
+      <div className="App">
+        <Header />
+        <Outlet />
 
-    </div>
+      </div>
+    </Provider>
   );
 };
 
---
+++ src/utils/cartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//slice 
+
+const cartSlice = createSlice({
+    name: "cart",
+
+    initialState: {
+        items: []
+    },
+
+    reducers: {
+        //add action whereever required 
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+
+        removeItem: (state) => {
+            state.items.pop();
+        },
+
+        clearItem: (state) => {
+            state.items.length = 0;
+        },
+    },
+
+});
+
+export const { addItem, removeItem, clearItem } = cartSlice.actions;
+
+export default cartSlice.reducer; 
